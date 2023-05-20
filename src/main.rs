5dd224fcdd2ccdd74277184af86fffae58ce56f8
Dhruva Srinivas <dhruvas17068@gmail.com>
2023-05-20 13:07:32 +0530

    feat: parse path input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use clap::Parser;
+
+use std::fs;
+use std::path;
+
+use std::io::ErrorKind;
+
+#[derive(Parser, Debug)]
+struct Args {
+    path: path::PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    match (args.path.is_dir(), args.path.is_file()) {
+        (true, false) => println!("{} is a directory", args.path.display()),
+        (false, true) => println!("{} is a file", args.path.display()),
+        _ => {
+            if let Err(e) = fs::read_dir(&args.path) {
+                match e.kind() {
+                    ErrorKind::NotFound => println!(
+                        "sz: error while reading path: `{}` not found",
+                        args.path.display()
+                    ),
+                    _ => println!("sz: error while reading path: {}", e),
+                }
+            }
+        }
+    }
 }
